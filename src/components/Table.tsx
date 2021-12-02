5b77d93ee99e56ec28f26171c6812f25e07a8bd0
Aji Hanggoro <adjihans@gmail.com>
2021-12-02 22:01:25 +0700

    add table body component

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -1,16 +1,56 @@
 import React from "react";
 import styled from "styled-components";
 
-const Table = () => {
+interface TableProps {
+  datas?: Data[];
+}
+
+interface Data {
+  username: string;
+  name: string;
+  email: string;
+  gender: string;
+  registeredDate: string;
+}
+
+const Table = (props: TableProps) => {
+  const {
+    datas = [
+      {
+        username: "tes",
+        name: "tes1",
+        email: "tes@mail.com",
+        gender: "male",
+        registeredDate: "tes",
+      },
+    ],
+  } = props;
+  const renderTableBody = () => {
+    if (!datas) return;
+    return (
+      <>
+        {datas.map((data: Data) => (
+          <CellRow>
+            <Cell>{data.username}</Cell>
+            <Cell>{data.name}</Cell>
+            <Cell>{data.email}</Cell>
+            <Cell>{data.gender}</Cell>
+            <Cell>{data.registeredDate}</Cell>
+          </CellRow>
+        ))}
+      </>
+    );
+  };
   return (
     <TableContainer>
-      <TableHeaderContainer>
+      <TableHeader>
         <Cell isHeader={true}>Username</Cell>
         <Cell isHeader={true}>Name</Cell>
         <Cell isHeader={true}>Email</Cell>
         <Cell isHeader={true}>Gender</Cell>
         <Cell isHeader={true}>Registered Date</Cell>
-      </TableHeaderContainer>
+      </TableHeader>
+      <TableBody>{renderTableBody()}</TableBody>
     </TableContainer>
   );
 };
@@ -28,9 +68,27 @@ const TableContainer = styled.div`
   margin: 2rem 0;
 `;
 
-const TableHeaderContainer = styled.div`
+const TableHeader = styled.div`
   width: 100%;
   height: 2rem;
+
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+`;
+
+const TableBody = styled.div`
+  width: 100%;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+const CellRow = styled.div`
+  width: 100%;
 
   display: flex;
   flex-direction: row;
